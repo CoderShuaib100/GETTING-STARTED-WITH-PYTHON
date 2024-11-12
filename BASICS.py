e03911be7a1685c292f1d80ebf20b4c73ccb0ec1
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-11-12 20:47:38 +0530

    first commit

---
+++ BASICS.py
@@ -0,0 +1,7 @@
+print(4)
+print("4")
+
+# escape sequence()
+
+print("Hello \nWorld!")
+print("Hello \tWorld!")
---
+++ appending basics.py
@@ -0,0 +1,7 @@
+name = "Muhammed Shuaib Sayeedi S.H"
+grade = 9
+School_Name = "Buvana Krishnan Matriculation Higher Secondary School"
+
+print("My name is", name,".")
+print("I am studying in", grade, "standard.")
+print("I am studying at", (School_Name + "."))
